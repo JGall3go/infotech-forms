a4d325fe71d4c80a70dd8c525baac6720b05747a
Juan Gallego <70003000+JGall3go@users.noreply.github.com>
2023-07-14 10:24:55 -0500

    Form Request

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import random
-
+import os
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
@@ -21,11 +21,21 @@ def acta_servicios():
     mservice = request.form.get("mservice")
     diagnostico = request.form.get("diagnostico")
     
-    evidencia = request.files['evidencia']
-    if evidencia.filename != '':
+    evidencia = request.files.getlist("evidencia")
 
-        random_bits = random.getrandbits(128)
-        img_hash = "%032x" % random_bits
-        evidencia.save(str(img_hash) + ".png")
+    imgs = []
 
-    return render_template('acta-servicios.html', data=[fecha_servicio, nombre_ing, nombre_client, sede, nombre_user, tel, ticket, mservice, diagnostico, img_hash])
+    if len(evidencia) > 0:
+        count = 0
+        for file in evidencia:
+            if count < 9:
+                random_bits = random.getrandbits(128)
+                img_hash = "%032x" % random_bits
+                file.save(os.path.join("static/evidences/", f"{str(img_hash)}.png"))
+                imgs.append(img_hash)
+                count+=1
+
+    return render_template('acta-servicios.html', data=[fecha_servicio, nombre_ing, nombre_client, sede, nombre_user, tel, ticket, mservice, diagnostico, imgs])
+
+if __name__ == '__main__':
+    app.run(debug = True)
