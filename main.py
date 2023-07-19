3c9be5479012b85ef92d64f16c3327876430db64
Juan Gallego <70003000+JGall3go@users.noreply.github.com>
2023-07-19 17:27:33 -0500

    Form Buttons

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, url_for, redirect
+from flask import Flask, request, render_template, session, url_for, redirect, send_file
 from pymongo import MongoClient
 import random
 import os
@@ -14,41 +14,13 @@ app.secret_key = "super secret key"
 
 @app.route('/')
 def hello():
-    return render_template('forms.html')
 
-@app.post('/acta-servicios')
-def acta_servicios():
+    if "username" in session:
 
-    fecha_servicio = request.form.get("fservicio")
-    nombre_ing = request.form.get("ning")
-    nombre_client = request.form.get("nclient")
-    sede = request.form.get("sede")
-    nombre_user = request.form.get("nuser")
-    tel = request.form.get("tel")
-    ticket = request.form.get("ticket")
-    mservice = request.form.get("mservice")
-    diagnostico = request.form.get("diagnostico")
+        return redirect(url_for("control_panel"))
     
-    # Se extraen todas las imagenes, se guardan localmente y se almacenan en una variable de tipo lista
-    evidencia = request.files.getlist("evidencia")
-    imgs = []
-
-    if len(evidencia) > 0:
-        count = 0
-        for file in evidencia:
-            if count < 9:
-                random_bits = random.getrandbits(128)
-                img_hash = "%032x" % random_bits
-                file.save(os.path.join("static/evidences/", f"{str(img_hash)}.png"))
-                imgs.append(img_hash)
-                count+=1
-
-    satisfaccion = request.form.get("satisfaccion")
-    tiempo_respuesta = request.form.get("tiempo_respuesta")
-    calidad_tecnica = request.form.get("calidad_tecnica")
-    calidad_humana = request.form.get("calidad_humana")
-
-    return render_template('user_templates/soporteti/acta-servicios.html', data=[fecha_servicio, nombre_ing, nombre_client, sede, nombre_user, tel, ticket, mservice, diagnostico, imgs, satisfaccion, tiempo_respuesta, calidad_tecnica, calidad_humana])
+    else:
+        return redirect(url_for("login"))
 
 @app.route('/login')
 def login():
@@ -110,6 +82,62 @@ def log_out():
     session.pop("username", None)
 
     return redirect(url_for("login"))
+
+# FORMS
+
+@app.route("/control-panel/forms/<form>/post", methods=['GET', 'POST'])
+def form_filling(form):
+
+    if "username" in session:
+
+        username = session["username"]
+        user = db["Users"].find_one({"username": username})
+        forms_list = user["forms-list"]
+        form_name = forms_list[form]
+
+        if request.method == 'POST':
+
+            fecha_servicio = request.form.get("fservicio")
+            nombre_ing = request.form.get("ning")
+            nombre_client = request.form.get("nclient")
+            sede = request.form.get("sede")
+            nombre_user = request.form.get("nuser")
+            tel = request.form.get("tel")
+            ticket = request.form.get("ticket")
+            mservice = request.form.get("mservice")
+            diagnostico = request.form.get("diagnostico")
+            
+            # Se extraen todas las imagenes, se guardan localmente y se almacenan en una variable de tipo lista
+            evidencia = request.files.getlist("evidencia")
+            imgs = []
+
+            if len(evidencia) > 0:
+                count = 0
+                for file in evidencia:
+                    if count < 9:
+                        random_bits = random.getrandbits(128)
+                        img_hash = "%032x" % random_bits
+                        file.save(os.path.join("static/evidences/", f"{str(img_hash)}.png"))
+                        imgs.append(img_hash)
+                        count+=1
+
+            satisfaccion = request.form.get("satisfaccion")
+            tiempo_respuesta = request.form.get("tiempo_respuesta")
+            calidad_tecnica = request.form.get("calidad_tecnica")
+            calidad_humana = request.form.get("calidad_humana")
+
+            return render_template('user_templates/Soporteti/acta-servicios.html', data=[fecha_servicio, nombre_ing, nombre_client, sede, nombre_user, tel, ticket, mservice, diagnostico, imgs, satisfaccion, tiempo_respuesta, calidad_tecnica, calidad_humana], username=username, form=form_name)
+
+        else:
+
+            return render_template(f"user_templates/{session['username']}/{form}=form.html")
+    else:
+        return redirect(url_for("login"))
+
+@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    path = "uploads/user.jpg"
+    return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
 
--- static/js/animations.js
+++ static/js/animations.js
@@ -2,22 +2,74 @@
 let forms = document.querySelectorAll(".form")
 
 for(let form in forms) {
+    
     let form_data = forms[form].querySelector(".form-data")
-    let button_form = form_data.querySelector(".button-form")
-    console.log(button_form)
 
-    anime({
-        targets: button_form,
-        translateY: '-55px',
-        easing: 'easeInOutQuad'
-    });
+    let button_form = form_data.querySelector(".button-form")
     let button_see = form_data.querySelector(".button-see")
     let button_download = form_data.querySelector(".button-download")
-}
+    let button_logs = form_data.querySelector(".button-logs")
 
-function button_animation() {
-    
-}
-//.addEventListener("mouseover", button_delay1);
+    forms[form].addEventListener("mouseenter", button_animation_over);
+    forms[form].addEventListener("mouseleave", button_animation_out);
 
-console.log(forms)
+    function button_animation_over() {
+
+        anime.remove(button_form);
+        anime.remove(button_see);
+        anime.remove(button_download);
+        anime.remove(button_logs);
+
+        anime({ // Button Form
+            targets: button_form,
+            translateY: '-65px',
+            delay: 50
+        });
+
+        anime({ // Button See
+            targets: button_see,
+            translateY: '-65px',
+            delay: 100
+        });
+
+        anime({ // Button Download
+            targets: button_download,
+            translateY: '-65px',
+            delay: 150
+        });
+
+        anime({ // Button Logs
+            targets: button_logs,
+            translateY: '-65px',
+            delay: 200
+        });
+    }
+
+    function button_animation_out() {
+
+        anime.remove(button_form);
+        anime.remove(button_see);
+        anime.remove(button_download);
+        anime.remove(button_logs);
+
+        anime({ // Button Form
+            targets: button_form,
+            translateY: '15px',
+        });
+
+        anime({ // Button See
+            targets: button_see,
+            translateY: '15px',
+        });
+
+        anime({ // Button Download
+            targets: button_download,
+            translateY: '15px',
+        });
+
+        anime({ // Button Logs
+            targets: button_logs,
+            translateY: '15px',
+        }); 
+    }
+}
